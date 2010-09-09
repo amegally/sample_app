edad34f0e8bbf99fc3776ba8581aa0d6a7c615b7
Alfred Megally <amegally@gmail.com>
2010-09-09 15:43:13 -0400

    users can now signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
-  def new
-    @title = "Sign up"
-    
+ 
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
   end
+ 
+ 
+  def new
+     @user = User.new
+     @title = "Sign up"
+    end
+    
+    def create
+       @user = User.new(params[:user])
+       if @user.save
+         # Handle a successful save.
+               flash[:success] = "Welcome to the Sample App!"
+               redirect_to @user
+       else
+         @title = "Sign up"
+         @user.password = nil
+         @user.password_confirmation = nil
+         
+         render 'new'
+       end
+     end
+ 
+  
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+   attr_accessible :name, :email, :password, :password_confirmation
+    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :name,  :presence => true,
+                      :length   => { :maximum => 50 }
+    validates :email, :presence => true,
+                      :format   => { :with => email_regex },
+                      :uniqueness => {:case_sensitive => false}
+   validates :password, :presence => true,
+                      :confirmation => true,
+                       :length => { :within => 6..40 }
+
+   before_save :encrypt_password
+
+    private
+
+      def encrypt_password
+        self.encrypted_password = encrypt(password)
+      end
+
+      def encrypt(string)
+        secure_hash("#{salt}--#{string}")
+      end
+
+      def make_salt
+        secure_hash("#{Time.now.utc}--#{password}")
+      end
+
+      def secure_hash(string)
+        Digest::SHA2.hexdigest(string)
+      end
+
+
+   
+end
